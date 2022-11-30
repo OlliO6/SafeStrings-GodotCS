cf3847b0b28dd4dcfc76dcf357c4c64408944f44
OlliDev <oliver.koenig.bi@gmail.com>
2022-12-01 00:21:04 +0100

    Delete unnecessary stuff

--- addons/SafeStrings/Generated/Res.g.cs
+++ addons/SafeStrings/Generated/Res.g.cs
@@ -6,9 +6,6 @@ public static class Res
 {
     public static readonly StringName SafeStringsCS_csproj = "res://SafeStringsCS.csproj";
     public static readonly StringName SafeStringsCS_sln = "res://SafeStringsCS.sln";
-    public static readonly StringName icon_svg = "res://icon.svg";
-    public static readonly StringName new_script_cs = "res://new_script.cs";
-    public static readonly StringName node_2d_tscn = "res://node_2d.tscn";
     public static readonly StringName project_godot = "res://project.godot";
     public static class addons
     {
