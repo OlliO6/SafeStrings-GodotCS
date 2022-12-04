6d99c90d48eed78db7981cfaebaecb05f5bf7df3
OlliDev <oliver.koenig.bi@gmail.com>
2022-12-04 14:12:16 +0100

    extend example

--- Examples/Player/Player.cs
+++ Examples/Player/Player.cs
@@ -7,12 +7,14 @@ public partial class Player : Node
 {
     // Prefer second way of getting coin scene because it caches and only loads once per game
     private static PackedScene coinScene1 = GD.Load<PackedScene>(Res.Examples.Items.Coin.coin_tscn);
-    private static PackedScene coinScene2 = Res.Examples.Items.Coin.coin_tscn.Value;
+    private static PackedScene coinScene2 = Res.Examples.Items.Coin.coin_tscn /*.Value*/;
 
     public static Player Instantiate()
     {
         // Relative to folder containing script.
         return Rel.player_tscn.Value.Instantiate<Player>();
+        // The same
+        return ((PackedScene)Rel.player_tscn).Instantiate<Player>();
     }
 
     public override void _Ready()
